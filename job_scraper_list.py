87fe8dd6ff34b879b06a2b2ec2b2612a1502adf3
Folajimi Adekoya <fadekoya@temenos.com>
2017-01-20 09:28:53 +0100

    trying to make things moduler

--- job_scraper_list.py
+++ job_scraper_list.py
@@ -31,17 +31,43 @@ def job_pages(max_pages):
 			if link.find('b'):
 				href = "https://it.jobindex.dk/jobsoegning"+ str(link.get('href')) 
 				title = link.string
-				print (href)
-				print (title)
+				console_screen(href)
+				open_tab(href)
+
 				#todo - add options print to screen
 				#- save to doc
 				#- save to spreadsheet
 				#- open in tab
-		page +=1
+	page +=1
+
+
+
+
+
+def console_screen():
+	print (href)
+	print (title)
+
+
+#funcion for tab opening
+def open_tab(links):
+	listLink=[]
+	listLink.append(links)
+
+	#store all links, but open the first 5
+
+#	numOpen = min(5, len(links))
+
+		
 
 
 #todo- number of pages should be via input screen
-job_pages(2)
+print("How many pages do you want to scrape jobs from ?")
+print("Please enter an integer:")	
+noOfPages=input()
+job_pages(int(noOfPages))
+
+# add exception handling for nonintegers and all other undesirable input
 #result_list_box > div > div.results.component--default > div:nth-child(1) > a:nth-child(2)
 	
 #	test = (linkElems[i].get('href'))
